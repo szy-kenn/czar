3e0c3ee47f02a526bbfd003213b15fba663e2db4
szy-kenn <johnkennethvelano@gmail.com>
2024-02-08 18:58:23 +0800

    added cz files

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    = 5;
+    = 6;
+}
+
+//  x: intt = 50
+//  int: int = 50
+// ^ expected "="
+
+//  int: int = 50
+// ^ reserved word, cannot be used as identifier name
+
+// int
+// ^ expected identifier before data type declaration (e.g. x: int)
+
+// [ T_INT ]
